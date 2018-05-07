2a413ecce09fd219acbcb5163240d4306c4ad8ca
Jay Pilgreen <jpilgreen@mcclatchy.com>
2018-05-07 14:21:32 -0500

    initial sliding nav test

--- assets/influencers.js
+++ assets/influencers.js
@@ -1,20 +1,39 @@
-if(window.IntersectionObserver && window.matchMedia("(min-width: 800px)").matches) {
-  let observer = new IntersectionObserver(handleIntersect);
+if(window.IntersectionObserver) {
   let influencers = document.querySelectorAll(".influencer");
 
-  for(let i = 0, len = influencers.length; i < len; i++) {
-    influencers[i].classList.add("animate-in");
-    observer.observe(influencers[i]);
-  }
-}
+  /**
+   * Project Nav
+   */
 
-function handleIntersect(entries, observer) {
-  entries.forEach(entry => {
-    if(entry.isIntersecting) {
-      requestAnimationFrame(() => {
-        entry.target.classList.remove("animate-in");
-        observer.unobserve(entry.target);
-      });
-    }
+  let navObserver = new IntersectionObserver(entries => {
+    let sentinel = entries[0];
+    let show = sentinel.isIntersecting || sentinel.boundingClientRect.y < 0;
+    document.querySelector(".project-flag").classList.toggle("down", show);
   });
+
+  navObserver.observe(influencers[3]);
+
+  /**
+   * Animation on cards
+   */
+
+  if(window.matchMedia("(min-width: 800px)").matches) {
+    let cardObserver = new IntersectionObserver(handleCardIntersect);
+
+    for(let i = 0, len = influencers.length; i < len; i++) {
+      influencers[i].classList.add("animate-in");
+      cardObserver.observe(influencers[i]);
+    }
+  }
+
+  function handleCardIntersect(entries, observer) {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        requestAnimationFrame(() => {
+          entry.target.classList.remove("animate-in");
+          observer.unobserve(entry.target);
+        });
+      }
+    });
+  }
 }
