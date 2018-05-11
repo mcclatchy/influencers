3158d8c97252b4fcef04920ce19d01317bfcf3df
Jay Pilgreen <jpilgreen@mcclatchy.com>
2018-05-11 13:57:18 -0500

    hugo swap

---
+++ Makefile
@@ -0,0 +1,19 @@
+all: carolinas east midwest west
+
+carolinas: carolinas.images
+	csv2json markets/$@.csv > data/$@.json
+
+east: east.images
+	csv2json markets/$@.csv > data/$@.json
+
+midwest: midwest.images
+	csv2json markets/$@.csv > data/$@.json
+
+west: west.images
+	csv2json markets/$@.csv > data/$@.json
+
+%.images:
+	mkdir -p static/img/$*/1200 static/img/$*/600
+	cd markets/$*/photos && mogrify -path ../../../static/img/$*/1200 \
+		-format 'jpg' -colorspace gray -brightness-contrast 3,20 -resize '1200x1200^' -quality 65 -strip * 
+	cd static/img/$*/1200 && mogrify -path ../600 -resize '600x600^' *
---
+++ static/js/influencers.js
@@ -0,0 +1,39 @@
+if(window.IntersectionObserver) {
+  let influencers = document.querySelectorAll(".influencer");
+
+  /**
+   * Project Nav
+   */
+
+  let navObserver = new IntersectionObserver(entries => {
+    let sentinel = entries[0];
+    let show = sentinel.isIntersecting || sentinel.boundingClientRect.y < 0;
+    document.querySelector(".project-flag").classList.toggle("down", show);
+  });
+
+  navObserver.observe(influencers[3]);
+
+  /**
+   * Animation on cards
+   */
+
+  if(window.matchMedia("(min-width: 800px)").matches) {
+    let cardObserver = new IntersectionObserver(handleCardIntersect);
+
+    for(let i = 0, len = influencers.length; i < len; i++) {
+      influencers[i].classList.add("animate-in");
+      cardObserver.observe(influencers[i]);
+    }
+  }
+
+  function handleCardIntersect(entries, observer) {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        requestAnimationFrame(() => {
+          entry.target.classList.remove("animate-in");
+          observer.unobserve(entry.target);
+        });
+      }
+    });
+  }
+}
